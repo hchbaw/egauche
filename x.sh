af5f39aa5ffa029ed842df0707a052a5994a8435
Takeshi Banse <takebi@laafc.net>
2010-10-11 17:06:41 +0900

    update.
    Signed-off-by: Takeshi Banse <takebi@laafc.net>

--- x.sh
+++ x.sh
@@ -1,19 +1,26 @@
 #!/bin/sh
 
-Xephyr :1 -noreset -ac -br -screen 800x600 -host-cursor &
+ps axo args|grep [X]ephyr
+if test $? -ne 0; then
+  #Xephyr :1 -noreset -ac -br -screen 800x600 -host-cursor &
+  Xephyr :1 -noreset -ac -br -screen 512x512 &
+  sleep 2
+fi
 
-sleep 2
 export DISPLAY=:1
 
 # fiddle my own envs.
 xset fp+ /pub/fonts/Pcf
 xset fp rehash
+setxkbmap -layout dvorak
 
 export RXVT_SOCKET=$HOME/.rxvt-unicode-${HOST}-testing
 unset WINDOW
 
 unset DBUS_SESSION_BUS_ADDRESS
-eval `dbus-session --exit-with-session`
+eval `dbus-launch --exit-with-session`
+
+urxvt -name 'URxvt-ml-tiny-simple' -g 80x25+20+40 &
 
 # start E.
 unset E_RESTART E_START E_IPC_SOCKET E_START_TIME E_CONF_PROFILE
